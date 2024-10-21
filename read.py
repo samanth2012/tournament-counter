d08d9b6abf0304bc37326614cae9ca4cdf5e0b36
Samanth <samp22@student.bth.se>
2024-10-21 11:05:00 +0000

    deleted

--- read.py
+++ read.py
@@ -1,6 +1,6 @@
 import time
 starttime=time.time()
-with open('C:\\Users\\vejam\\tournament-counter\\p6.pgn','r') as file:
+with open('p6.pgn','r') as file:
     s=file.read()
     count1=s.count("tournament")
     time1=time.time()-starttime
